2073209a1080c9205f51516ea5bc33e7ae97678f
Enoka <eslip96@gmail.com>
2023-03-25 16:11:45 -0600

    starting javascript function

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const rootEl = document.getElementsByClassName("title");
+const rootElSelector = document.querySelector("h2");
+
+console.log(rootEl);
+console.log(rootElSelector);
+
+const header = document.createElement("h1");
+const headerText = document.createTextNode("Hey howdy, how you durn???");
+
+console.log(header);
+console.log(headerText);
+
+function callStudent {
+  
+}
